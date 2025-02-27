faa388d4ce6270994600430208fd61aedfe43429
wvenialbo <wvenialbo@gmail.com>
2025-02-27 06:20:58 -0300

    refactor(typing): remove module
    Remove unused type utilities module.

--- src/goesdr/typing/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Provide utilities for type validation and type hinting generation."""
-
-from .type_hinting import get_value_typehint as typehint
-from .type_validation import validate_value_type as validate
-
-__all__ = ["typehint", "validate"]
