7659bb83eea38859d3c135a617bf7152819548d5
wvenialbo <wvenialbo@gmail.com>
2025-02-26 23:13:54 -0300

    feat(goesdr): add initial package
    Add initial package for GOES-R dataset reading functionality.

---
+++ src/goesdr/__init__.py
@@ -0,0 +1,5 @@
+"""Provide functionality for reading fragments of GOES-R datasets."""
+
+__package_id__ = "GOES-DR"
+__package_name__ = f"{__package_id__} — GOES Satellite Imagery Dataset Reader"
+__version__ = "v0.1-rc1"
