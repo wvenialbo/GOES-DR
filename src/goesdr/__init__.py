05ebf74a5ccb7926b6b58d73aca30e8f4279789c
wvenialbo <wvenialbo@gmail.com>
2025-02-27 08:36:44 -0300

    feat(init): export utility functions and class
    Expose Datarecord and field-related classes in __all__ for public
    access.

--- src/goesdr/__init__.py
+++ src/goesdr/__init__.py
@@ -1,5 +1,16 @@
 """Provide functionality for reading fragments of GOES-R datasets."""
 
+from .datarecord import Datarecord
+from .fields import dimension, field, record, variable
+
+__all__ = [
+    "Datarecord",
+    "dimension",
+    "field",
+    "record",
+    "variable",
+]
+
 __package_id__ = "GOES-DR"
 __package_name__ = f"{__package_id__} — GOES Satellite Imagery Dataset Reader"
 __version__ = "v0.1-rc1"
