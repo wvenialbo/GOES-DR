8084ce6fd268c19a8a9435007ab7c200667372ba
wvenialbo <wvenialbo@gmail.com>
2025-03-02 04:17:00 -0300

    feat(grid): add grid_noaa module
    Add latitude and longitude grid calculation using NOAA's algorithm.

---
+++ src/goesdr/grid_noaa.py
@@ -0,0 +1,109 @@
+"""
+Calculate latitude and longitude grids data.
+
+Calculate latitude and longitude from GOES ABI fixed grid projection
+data. GOES ABI fixed grid projection is a map projection relative to the
+GOES satellite.
+
+Units: latitude in °N (°S < 0), longitude in °E (°W < 0)
+
+See GOES-R Product User Guide (PUG) Volume 5 (L2 products) Section 4.2.8
+for details & example of calculations.
+
+Functions
+---------
+calculate_latlon_noaa
+    Calculate latitude and longitude grids using NOAA algorithm.
+"""
+
+from netCDF4 import Dataset  # pylint: disable=no-name-in-module
+from numpy import (
+    arctan,
+    cos,
+    errstate,
+    float32,
+    isnan,
+    meshgrid,
+    nan,
+    pi,
+    power,
+    sin,
+    sqrt,
+    where,
+)
+from numpy.typing import NDArray
+
+
+def calculate_latlon_grid_noaa(
+    file_id: Dataset,
+) -> tuple[NDArray[float32], NDArray[float32]]:
+    """
+    Calculate latitude and longitude grids using NOAA algorithm.
+
+    Parameters
+    ----------
+    file_id : Dataset
+        The netCDF dataset containing ABI L1b or L2 data. It is .nc file
+        opened using the netCDF4 library.
+    """
+
+    # Read in GOES ABI fixed grid projection variables and constants
+    x_coordinate_1d = file_id.variables["x"][
+        :
+    ]  # E/W scanning angle in radians
+    y_coordinate_1d = file_id.variables["y"][
+        :
+    ]  # N/S elevation angle in radians
+    projection_info = file_id.variables["goes_imager_projection"]
+    lon_origin = projection_info.longitude_of_projection_origin
+    H = (
+        projection_info.perspective_point_height
+        + projection_info.semi_major_axis
+    )
+    r_eq = projection_info.semi_major_axis
+    r_pol = projection_info.semi_minor_axis
+
+    # Create 2D coordinate matrices from 1D coordinate vectors
+    x_coordinate_2d, y_coordinate_2d = meshgrid(
+        x_coordinate_1d, y_coordinate_1d
+    )
+
+    # Equations to calculate latitude and longitude
+    lambda_0 = (lon_origin * pi) / 180.0
+    a_var = power(sin(x_coordinate_2d), 2.0) + (
+        power(cos(x_coordinate_2d), 2.0)
+        * (
+            power(cos(y_coordinate_2d), 2.0)
+            + (
+                ((r_eq * r_eq) / (r_pol * r_pol))
+                * power(sin(y_coordinate_2d), 2.0)
+            )
+        )
+    )
+
+    # Ignore numpy errors for sqrt of negative number; occurs for
+    # GOES-16 ABI CONUS sector data
+    with errstate(invalid="ignore"):
+        b_var = -2.0 * H * cos(x_coordinate_2d) * cos(y_coordinate_2d)
+        c_var = (H**2.0) - (r_eq**2.0)
+        r_s = (-1.0 * b_var - sqrt((b_var**2) - (4.0 * a_var * c_var))) / (
+            2.0 * a_var
+        )
+        s_x = r_s * cos(x_coordinate_2d) * cos(y_coordinate_2d)
+        s_y = -r_s * sin(x_coordinate_2d)
+        s_z = r_s * cos(x_coordinate_2d) * sin(y_coordinate_2d)
+
+        abi_lat = (180.0 / pi) * (
+            arctan(
+                ((r_eq * r_eq) / (r_pol * r_pol))
+                * (s_z / sqrt(((H - s_x) * (H - s_x)) + (s_y * s_y)))
+            )
+        )
+        abi_lon = (lambda_0 - arctan(s_y / (H - s_x))) * (180.0 / pi)
+
+    is_valid = ~(isnan(abi_lat) | isnan(abi_lon))
+
+    abi_lat = where(is_valid, abi_lat, nan)
+    abi_lon = where(is_valid, abi_lon, nan)
+
+    return abi_lat, abi_lon
