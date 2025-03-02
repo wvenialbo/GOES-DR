47f70a05e1b57d77d883f45527d992724cafdb55
wvenialbo <wvenialbo@gmail.com>
2025-03-02 07:02:48 -0300

    feat(grid): add type alias for boolean array

--- src/goesdr/grid/array.py
+++ src/goesdr/grid/array.py
@@ -1,11 +1,7 @@
-from numpy import (
-    dtype,
-    float32,
-    float64,
-    ndarray,
-)
+from numpy import bool_, dtype, float32, float64, ndarray
 from numpy.ma import MaskedArray
 
+ArrayBool = ndarray[tuple[int, ...], dtype[bool_]]
 ArrayFloat32 = ndarray[tuple[int, ...], dtype[float32]]
 ArrayFloat64 = ndarray[tuple[int, ...], dtype[float64]]
 
