87be04a7641093ed1aa5971ab7cba09e07a18f95
wvenialbo <wvenialbo@gmail.com>
2025-03-06 16:45:08 -0300

    refactor(image): update fields semantics

--- src/goesdr/image.py
+++ src/goesdr/image.py
@@ -15,10 +15,9 @@ GOESImageMetadata
 from numpy import bool_, float32, int8, int16, int32, uint16
 from numpy.typing import NDArray
 
-from .netcdf import DatasetView, make_variable, scalar
-from .netcdf.fields import VariableType
+from .netcdf import DatasetView, variable
 
-cmip: VariableType = make_variable("CMI", array=True)
+cmip = variable("CMI")
 
 
 class GOESImage(DatasetView):
@@ -42,12 +41,12 @@ class GOESImage(DatasetView):
         The fill value used for missing or invalid data points.
     """
 
-    band_id: int32 = scalar()
-    band_wavelength: float32 = scalar()
+    band_id: int32 = cmip.scalar()
+    band_wavelength: float32 = cmip.scalar()
 
-    data: NDArray[float32] = cmip()
-    mask: NDArray[bool_] = cmip()
-    fill_value: uint16 = cmip()
+    data: NDArray[float32] = cmip.array()
+    mask: NDArray[bool_] = cmip.array(entry="mask")
+    fill_value: uint16 = cmip.array(entry="fill_value")
 
 
 class GOESImageMetadata(DatasetView):
@@ -76,12 +75,12 @@ class GOESImageMetadata(DatasetView):
         The grid mapping information.
     """
 
-    long_name: str = cmip()
-    standard_name: str = cmip()
-    sensor_band_bit_depth: int8 = cmip()
-    valid_range: NDArray[int16] = cmip()
-    scale_factor: float32 = cmip()
-    add_offset: float32 = cmip()
-    units: str = cmip()
-    resolution: str = cmip()
-    grid_mapping: str = cmip()
+    long_name: str = cmip.attribute()
+    standard_name: str = cmip.attribute()
+    sensor_band_bit_depth: int8 = cmip.attribute()
+    valid_range: NDArray[int16] = cmip.attribute()
+    scale_factor: float32 = cmip.attribute()
+    add_offset: float32 = cmip.attribute()
+    units: str = cmip.attribute()
+    resolution: str = cmip.attribute()
+    grid_mapping: str = cmip.attribute()
